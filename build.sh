4ded4c9e2556c7b436fd298d6a99206c7936d512
Aiden McClelland <me@drbonez.dev>
2023-09-21 17:46:09 -0600

    add start to /etc/hosts

--- build.sh
+++ build.sh
@@ -98,8 +98,8 @@ fi
 mkdir -p config/includes.chroot/etc
 echo start > config/includes.chroot/etc/hostname
 cat > config/includes.chroot/etc/hosts << EOT
-127.0.0.1       localhost
-::1             localhost ip6-localhost ip6-loopback
+127.0.0.1       localhost start
+::1             localhost start ip6-localhost ip6-loopback
 ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters
 EOT
